a4be1d672b0c25d58c9e2b8a8a0638bd40d38936
Hana Kluch Duchková <hana.duchkova@icloud.com>
2022-02-08 07:21:02 +0100

    cvičení 4

--- src/components/Registrace/index.jsx
+++ src/components/Registrace/index.jsx
@@ -6,14 +6,17 @@ const Registrace = () => {
   const [errorVisible, setErrorVisible] = useState(false);
   const [submitDisabled, setSubmitDisabled] = useState(true);
   const [registraceOk, setRegistraceOk] = useState(false);
+  const [checked, setChecked] = useState(false);
   const [country, setCountry] = useState("Česká republika");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (userName === "") {
+    if (userName === "" && checked === false) {
       setErrorVisible(true);
       setRegistraceOk(false);
-    } else {
+      setSubmitDisabled(true);
+    }
+    else {
       setErrorVisible(false);
       setRegistraceOk(true);
     }
@@ -22,40 +25,70 @@ const Registrace = () => {
   const handleChange = (event) => {
     setUserName(event.target.value);
     setSubmitDisabled(false);
-    if(event.target.value === ""){
-        setSubmitDisabled(true);
-        setErrorVisible(true);
-        setRegistraceOk(false);
+    if (event.target.value === "" && !checked) {
+      setSubmitDisabled(true);
+      setErrorVisible(true);
+      setRegistraceOk(false);
+    }
+    else if(event.target.value === "" || !checked){
+      setSubmitDisabled(true);
+      setErrorVisible(true);
+      setRegistraceOk(false);
+    }
+    else if (event.target.value === string){
+        setSubmitDisabled(false);
     }
   };
 
   const handleSelect = (event) => {
-    setCountry(event.target.value)
-  }
+    setCountry(event.target.value);
+  };
+
+  const handleCheckbox = () => {
+    if(!checked){
+        setChecked(true);
+        setSubmitDisabled(false);
+    }
+    else if(checked){
+        setChecked(false);
+        setSubmitDisabled(true);
+    }
+
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Uživatelské jméno:
-        <input type="text" onChange={handleChange} value={userName} />
-      </label>
-      <button type="submit" disabled={submitDisabled}>
-        Registrovat
-      </button>
+      <div>
+        {" "}
+        <label>
+          Uživatelské jméno:
+          <input type="text" onChange={handleChange} value={userName} />
+        </label>
+        <button type="submit" disabled={submitDisabled}>
+          Registrovat
+        </button>
+      </div>
       <div className={errorVisible ? "error-visible" : "error-hidden"}>
         Uživatelské jméno je povinný údaj
       </div>
-      <label>
-        Země původu:
-        <select onChange={handleSelect} value={country}>
-          <option value="Chorvatsko">Chorvatsko</option>
-          <option value="Česká republika">Česká republika</option>
-          <option value="Polsko">Polsko</option>
-          <option value="Slovenská republika">Slovenská republika</option>
-        </select>
-      </label>
-      <div className={registraceOk? "registrace-ok" : "registrace-hidden"}>
-      Registrován nový uživatel {userName} ze země {country}
+      <div>
+        <label>
+          Země původu:
+          <select onChange={handleSelect} value={country}>
+            <option value="Chorvatsko">Chorvatsko</option>
+            <option value="Česká republika">Česká republika</option>
+            <option value="Polsko">Polsko</option>
+            <option value="Slovenská republika">Slovenská republika</option>
+          </select>
+        </label>
+      </div>
+
+      <div className={registraceOk ? "registrace-ok" : "registrace-hidden"}>
+        Registrován nový uživatel {userName} ze země {country}
+      </div>
+      <div>
+        <input type="checkbox" onChange={handleCheckbox} value={checked} />{" "}
+        <label>Souhlasím s obchodními podmínkami</label>
       </div>
     </form>
   );
